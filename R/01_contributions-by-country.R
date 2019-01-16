3ae3602340038d405053b91c30e8d23c174acc0e
Luis Verde Arregoitia <luisd@ciencias.unam.mx>
2019-01-16 11:45:17 -0300

    Update 01_contributions-by-country.R

--- R/01_contributions-by-country.R
+++ R/01_contributions-by-country.R
@@ -1,59 +1,74 @@
-# load packages
+# Contributions by country
+
+# packages
 library(rnaturalearth)
 library(rgeos)
 library(ggplot2)
 library(dplyr)
-library(here)
 library(stringi)
 library(ggthemes)
 library(maptools)
 library(sf)
 library(scico)
 library(extrafont)
+library(here)
+library(ggspatial)
 
-# register fonts
-loadfonts()
+
+# fonts
+loadfonts(device = "postscript")
 # get a world map
-NE_countries <- ne_countries(scale = "small", type = "countries", returnclass = 'sp')
+NE_countries <- ne_countries(scale = "small", type = "countries", returnclass = "sp")
+
 # recenter
-world <- nowrapRecenter(NE_countries, offset = 180-150)
+world <- nowrapRecenter(NE_countries, offset = 30)
 
 # to simple features, then prepare country variable
 worldsf <- st_as_sf(world)
-worldsf <-  worldsf %>% mutate(country=NE_countries@data$name_long)
+worldsf <- worldsf %>% mutate(country = NE_countries@data$name_long)
 
 # read the data
-absflat <- read.csv(here("data","abstracts.csv"),stringsAsFactors = FALSE)
+absflat <- read.csv(here("data", "absFlatNov2017.csv"), stringsAsFactors = FALSE)
 # count pr. auth. countries
-ctryCount <- absflat %>% count(author_country)
+ctryCount <- absflat %>% count(authorCountry)
 # manual edits so that country names match
-ctryCount$author_country <- 
-  stri_replace_all_fixed(str = ctryCount$author_country, 
-                         pattern = c("Russia","South Korea","The Netherlands","USA","UK"),
-                         replacement = c("Russian Federation","Republic of Korea","Netherlands","United States","United Kingdom"), vectorize_all = FALSE)
+ctryCount$authorCountry <-
+  stri_replace_all_fixed(
+    str = ctryCount$authorCountry,
+    pattern = c("Russia", "South Korea", "The Netherlands", "USA", "UK"),
+    replacement = c("Russian Federation", "Republic of Korea", "Netherlands", "United States", "United Kingdom"), vectorize_all = FALSE
+  )
 
-# merge 
-worldMapCount <- left_join(worldsf,ctryCount,by=c("country"="author_country"))
+# merge
+worldMapCount <- left_join(worldsf, ctryCount, by = c("country" = "authorCountry"))
 
-# project (Robinson)
+# project
 worldMapCountRob <- st_transform(x = worldMapCount, crs = "+proj=robin +lon_0=150 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
+worldRobF <- worldMapCountRob %>% filter(country != "Greenland", country != "Antarctica")
 
-fig1 <- 
-worldMapCountRob %>%  filter(country!="Greenland",country!="Antarctica") %>% 
-    ggplot()+
-    geom_sf(aes(fill=n),color = "#3B4252",size=0.12)+
-    scale_fill_scico(palette = "davos", name="Number of contributions",direction = -1,end = 0.89)+
-    coord_sf(datum = NA) + 
-    theme(plot.background = element_rect(color = "black",size=0.5)) +
-    theme(panel.background = element_rect(fill = "#f1f2f3", color = "#f1f2f3"))+
-    theme(
-      panel.grid = element_blank(), 
-      line = element_blank(), 
-      rect = element_blank(),
-      text = element_text(family="Roboto"))+
-    theme(legend.direction = "horizontal",
-          legend.position = "bottom") +
-    guides(fill=guide_colorbar(frame.colour = "black")) 
-
-# export
-#ggsave(fig1,filename = here("figures","fig1.pdf"),width = 7,height =4)
+# plot
+ggplot(worldRobF) +
+  geom_sf(aes(fill = n), color = "#3B4252", size = 0.12) +
+  scale_fill_scico(
+    palette = "davos", name = "Number of abstracts",
+    direction = -1, end = 0.89, breaks = c(20, 100, 200)
+  ) +
+  guides(fill = guide_colorbar(
+    frame.colour = "black",
+    title.vjust = 0.25,
+    label.position = "top"
+  )) +
+  theme(
+    panel.grid.major.y = element_line(color = "red"),
+    plot.background = element_blank(),
+    panel.background = element_rect(fill = "#f1f2f3", color = "#f1f2f3"),
+    rect = element_blank(),
+    text = element_text(family = "Roboto"),
+    legend.direction = "horizontal",
+    legend.position = "bottom", legend.text = element_text(size = 7)
+  ) +
+  coord_sf() +
+  annotation_north_arrow(
+    location = "bl", height = unit(0.6, "cm"), width = unit(0.6, "cm"),
+    style = north_arrow_orienteering
+  )
